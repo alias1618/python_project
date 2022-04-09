59d5ecfbe4a748ab9d7c9b7f4add9f456762d2c9
DESKTOP-BKTE25U\home <wei.liang.hong84@gmail.com>
2022-04-09 16:15:20 +0800

    20220409_0415

--- web2/main.py
+++ web2/main.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
     return render_template("a2.html")
+
+@app.route("/a3/")
+def index1():
+    return render_template("a3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
